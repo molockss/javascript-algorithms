6bb43156841675434d2360d731ed11876ce902d1
Mahamed Osman <mahamed_67@hotmail.com>
2022-12-28 14:15:42 +0400

    Vowel Counter Algorithm Challenge

--- vowelsCounter/index-START.js
+++ vowelsCounter/index-START.js
@@ -31,3 +31,21 @@ e.g vowelsCounter('anehizxcv') // will return 3
     
         return sum 
     }
+
+
+//     /*
+//  Using Regular Expressions (Regex) to count the number of vowels in a string of text.
+//  */
+
+// function vowelsCounter(text) {
+//     // Search text with Regex and store all matching instances 
+//     let matchingInstances = text.match(/[aeiou]/gi);
+
+//     // Check if matching instances exist then calculate length
+//     if (matchingInstances) {    
+//         // Return number of vowels
+//         return matchingInstances.length
+//     } else {
+//         return 0
+//     }
+// }
